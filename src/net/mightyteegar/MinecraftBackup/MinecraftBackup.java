d0a30fbadbfe899f75fca9c2113b32787dd7f323
dave baker <baneofspam@gmail.com>
2015-02-01 08:26:41 -0500

    Updated header with thanks to Asheville

--- src/net/mightyteegar/MinecraftBackup/MinecraftBackup.java
+++ src/net/mightyteegar/MinecraftBackup/MinecraftBackup.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Special thanks to the breweires of Asheville for inspiration.  
  */
 package net.mightyteegar.MinecraftBackup;
 
